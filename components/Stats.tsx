d9cf08f1158a00c13caa7b2c0b7089e793af10a2
NatsuDzn <natsuxgraph@gmail.com>
2022-10-06 22:15:56 +0200

    ✨ feat(stats): add a warning when stats are empty

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -1,4 +1,5 @@
-import { Progress } from "@mantine/core";
+import { Alert, Progress } from "@mantine/core";
+import { InfoCircle } from "tabler-icons-react";
 import { useBingo } from "../context/state";
 
 function Stats() {
@@ -9,28 +10,39 @@ function Stats() {
   }).length;
 
   const percentage: number = Number(
-    ((markedAsDoneCount/ value.mediaList.length) * 100).toFixed(1)
+    ((markedAsDoneCount / value.mediaList.length) * 100).toFixed(1)
   );
 
   return (
-    <Progress
-      size="xl"
-      radius="xl"
-      sections={[
-        {
-          value: percentage,
-          color: "green",
-          label: `${String(percentage)} %`,
-          tooltip: `Completed (${percentage} %) - ${markedAsDoneCount} medias`,
-        },
-        {
-          value: 100 - percentage,
-          color: "blue",
-          label: `${String(100 - percentage)} %`,
-          tooltip: `Not completed (${100 - percentage} %) - ${ value.mediaList.length - markedAsDoneCount} medias`,
-        },
-      ]}
-    />
+    <>
+      {markedAsDoneCount > 0 && percentage !== NaN ? (
+        <Progress
+          size="xl"
+          radius="xl"
+          sections={[
+            {
+              value: percentage,
+              color: "green",
+              label: `${String(percentage)} %`,
+              tooltip: `Completed (${percentage} %) - ${markedAsDoneCount} medias`,
+            },
+            {
+              value: 100 - percentage,
+              color: "blue",
+              label: `${String(100 - percentage)} %`,
+              tooltip: `Not completed (${100 - percentage} %) - ${
+                value.mediaList.length - markedAsDoneCount
+              } medias`,
+            },
+          ]}
+        />
+      ) : (
+        <Alert icon={<InfoCircle size={16} />} title="Oops!" color="orange">
+          Your bingo is either empty or you don&apos;t have any completed
+          entries !
+        </Alert>
+      )}
+    </>
   );
 }
 
